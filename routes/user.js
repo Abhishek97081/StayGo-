ed69831978af342a9f7c369fb969a1bce88ad7f7
Abhishek97081 <abhisinghrajput970@gmail.com>
2026-05-06 14:27:41 +0530

    logout bug solved

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const User = require("../models/user.js");
+
 const { saveRedirectUrl } = require("../middleware.js");
-const user = require("../models/user.js");
 const userController = require("../controllers/user.js");
+
 const wrapAsync = (fn) => {
   return function (req, res, next) {
     fn(req, res, next).catch(next);
-  };};
+  };
+};
 
 router.get("/signup", userController.renderSignupForm);
 
 router.post("/signup", wrapAsync(userController.signup));
 
-router.get("/login",userController.renderLoginForm);
+router.get("/login", userController.renderLoginForm);
 
 router.post(
   "/login",
@@ -26,7 +27,8 @@ router.post(
   userController.login
 );
 
+// Logout routes
 router.get("/logout", userController.logout);
-
+router.delete("/logout", userController.logout);
 
 module.exports = router;
